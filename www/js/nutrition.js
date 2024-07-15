783fbecca2612aa9599a15e0db1215b51c9d82c0
bcfeszjing <bcfeszjing@gmail.com>
2024-07-15 15:59:20 +0800

    addFood function no design

--- www/js/nutrition.js
+++ www/js/nutrition.js
@@ -3,21 +3,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const dateInput = document.getElementById('date');
     const today = new Date().toISOString().split('T')[0];
     dateInput.value = today;
-    displayFoodList();
+    displayNutritionDetails();
 });
 
-function displayFoodList() {
+function displayNutritionDetails() {
     const selectedDate = document.getElementById('date').value;
     const savedFoods = JSON.parse(localStorage.getItem('savedFoods')) || [];
     const categories = ['breakfast', 'lunch', 'dinner', 'snack'];
 
     categories.forEach(category => {
         const categoryContainer = document.getElementById(category).querySelector('.food-items');
+        const totalKcalSpan = document.getElementById(`${category}-kcal`);
         categoryContainer.innerHTML = '';
+
+        let totalKcal = 0;
 
         const foods = savedFoods.filter(food => food.date === selectedDate && food.category === category);
 
         foods.forEach(food => {
+            totalKcal += parseFloat(food.nutrition.calories);
+
             const foodItem = document.createElement('div');
             foodItem.className = 'food-item';
             foodItem.innerHTML = `
@@ -26,7 +31,15 @@ function displayFoodList() {
             `;
             categoryContainer.appendChild(foodItem);
         });
+
+        totalKcalSpan.innerText = totalKcal.toFixed(2);
     });
+}
+
+function goToAddFoodDetails(category) {
+    const selectedDate = document.getElementById('date').value;
+    const queryString = `date=${encodeURIComponent(selectedDate)}&category=${encodeURIComponent(category)}`;
+    window.location.href = `addFoodDetails.html?${queryString}`;
 }
 
 function viewFoodDetails(foodName, date, category) {
